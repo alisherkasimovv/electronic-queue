014f39b15faf753bd10d3a06ee56ac57871c2958
Alisher Kasimov <alisherkasimov1994@gmail.com>
2019-10-17 15:24:10 +0500

    User model created

---
+++ src/main/java/uz/queue/models/Employees.java
@@ -0,0 +1,44 @@
+package uz.queue.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employees {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "serial")
+    private int id;
+
+    // Username of the employee and it should be unique.
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @Column(name = "password", unique = true)
+    private String password;
+
+    @Nullable
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Nullable
+    @Column(name = "last_name")
+    private String lastName;
+
+    // User portrait. This could be null
+    @Nullable
+    @Column(name = "portrait", columnDefinition = "TEXT")
+    private String portrait;
+
+}
